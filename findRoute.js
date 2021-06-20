0d05e95a7b69deb026e280e57376f12de554358c
ashot08 <albertguzeev@yandex.ru>
2021-06-20 13:46:26 +0700

    findRoute function: add code

--- findRoute.js
+++ findRoute.js
@@ -13,6 +13,8 @@ const distances = [
     {source: 2, destination: 4, distance: 10},
     {source: 2, destination: 6, distance: 100},
     {source: 2, destination: 6, distance: 25},
+    {source: 3, destination: 1, distance: 5},
+    {source: 6, destination: 2, distance: 25},
 ]
 
 const buildGraph = (distances) => {
@@ -48,6 +50,41 @@ const distancesGraph = {
         [6, 25],
     ],
 }
-const findRoute = (cities, distances, customerCity) => {
+const findRoute = (citiesWithStore, distances, customerCity) => {
+    const graph = buildGraph(distances);
+    let currentCity = customerCity;
+    const routesCount = Object.keys(graph).length;
+    let citiesRoutesData = Object.create(null);
+    citiesRoutesData[customerCity] = {
+        previousCity: 0,
+        totalWeight: 0,
+    }
+    // citiesRoutesData[customerCity] = {
+    //     previousCity: customerCity,
+    //     totalWeight: 0,
+    // }
+    for(let i = 0; i < routesCount; i++){
+        let shortest = {cityId: currentCity, distance: Infinity};
+        for(let direction of graph[currentCity]){
+            if(direction[1] < shortest.distance){
+                shortest.distance = direction[1];
+                shortest.cityId = direction[0];
+            }
+            let route = citiesRoutesData[direction[0]] || {};
+            let weight = direction[1] + citiesRoutesData[customerCity].totalWeight;
 
+            if( route.totalWeight === undefined || route.totalWeight > weight){
+                route.previousCity = currentCity;
+                route.totalWeight = weight;
+                citiesRoutesData[direction[0]] = route;
+            }
+        }
+        delete graph[currentCity];
+        currentCity = shortest.cityId;
+
+
+        console.log(graph);
+    }
+    console.log(citiesRoutesData);
 }
+findRoute(cities, distances,1);
